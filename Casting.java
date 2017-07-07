238f8c9904517cda0715ddfe3b7c1ed100c75576
Muriel Santos <muriel.sena@gmail.com>
2017-07-07 16:53:49 -0300

    imprimindo valores

--- Casting.java
+++ Casting.java
@@ -2,5 +2,8 @@ public class Casting {
 	public static void main(String[] args) {
 		int numero = 10;
 		double pi = 3.145;
+
+		System.out.println("Numero: " + numero);
+		System.out.println("PI: " + pi);
 	}
 }
